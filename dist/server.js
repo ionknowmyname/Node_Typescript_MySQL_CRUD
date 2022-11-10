2424eba3821e1c62bed069d6bf1170b23023100d
Faithful <faithfulolaleru09@gmail.com>
2022-11-10 18:05:58 +0100

    register user with stored procedure working

--- dist/server.js
+++ dist/server.js
@@ -4,14 +4,103 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const mysql_1 = __importDefault(require("mysql"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const app = (0, express_1.default)();
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use(express_1.default.json());
-app.get('/', (req, res) => {
-    res.send('Its working');
+app.get('/details/:id', (req, res, next) => {
+    var pool = mysql_1.default.createPool({
+        host: process.env.HOST,
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        database: process.env.DATABASE,
+        connectionLimit: 10,
+        multipleStatements: true
+    });
+    pool.getConnection((err, conn) => {
+        if (err) {
+            console.log('Entered an error: ', err);
+            res.send({
+                success: false,
+                statusCode: 500,
+                message: 'Error during connection'
+            });
+            return;
+        }
+        console.log('id from req.params: ' + req.params.id);
+        conn.query('SELECT * FROM customers WHERE customerNumber=?', [req.params.id], (err, rows) => {
+            if (err) {
+                console.log('Encountered an error: ', err);
+                conn.release();
+                return res.send({
+                    success: false,
+                    statusCode: 400
+                });
+            }
+            if (rows.length < 1) {
+                return res.send({
+                    message: 'Data not found',
+                    statusCode: 404,
+                });
+            }
+            res.send({
+                message: 'Successul',
+                statusCode: 200,
+                data: rows
+            });
+            conn.release(); // close connection
+        });
+    });
 });
-app.post('/', (req, res) => {
-    res.send({ data: req.body });
+app.post('/register/', (req, res) => {
+    var pool = mysql_1.default.createPool({
+        host: process.env.HOST,
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        database: process.env.DATABASE,
+        connectionLimit: 10,
+        multipleStatements: true
+    });
+    pool.getConnection((err, conn) => {
+        if (err) {
+            console.log('Entered an error: ', err);
+            res.send({
+                success: false,
+                statusCode: 500,
+                message: 'Error during connection'
+            });
+            return;
+        }
+        console.log('req.body: ' + req.body);
+        let sqlQuery = `call registeruser(?,?,?)`;
+        conn.query(sqlQuery, [req.body.email, req.body.phone, req.body.password], (err, rows) => {
+            if (err) {
+                console.log('Encountered an error: ', err);
+                conn.release();
+                return res.send({
+                    success: false,
+                    statusCode: 400
+                });
+            }
+            res.send({
+                message: 'Successul',
+                statusCode: 200,
+                // data: rows
+            });
+            conn.release(); // close connection
+        });
+    });
+});
+app.post('/id/:id/name/:name', (req, res) => {
+    res.send({
+        data: req.body,
+        params: {
+            id: req.params.id,
+            name: req.params.name
+        }
+    });
 });
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
-import express, { NextFunction } from 'express';
-import { Request, Response } from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import mysql from 'mysql';
+import dotenv from 'dotenv'
+
+dotenv.config();
 
 const app = express();
 
@@ -10,10 +12,10 @@ app.use(express.json());
 app.get('/details/:id', (req: Request, res: Response, next: NextFunction) => {
 
   var pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'node-typescript',
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
     connectionLimit: 10,
     multipleStatements: true
   });
@@ -34,13 +36,20 @@ app.get('/details/:id', (req: Request, res: Response, next: NextFunction) => {
 
     conn.query('SELECT * FROM customers WHERE customerNumber=?', [req.params.id], (err: any, rows: any) => {
       if(err){
-        console.log('Entered an error: ', err);
+        console.log('Encountered an error: ', err);
         conn.release();
 
         return res.send({
           success: false,
           statusCode: 400
         });      
+      }
+
+      if(rows.length < 1){
+        return res.send({
+          message: 'Data not found',
+          statusCode: 404,
+        });
       }
 
       res.send({
@@ -53,15 +62,57 @@ app.get('/details/:id', (req: Request, res: Response, next: NextFunction) => {
     });
     
   });
+}); 
 
-  // former impl
+app.post('/register/', (req: Request, res: Response) => {
 
-  /* res.send({
-    message: 'Its working',
-    id: req.params.id,
-    name: req.params.name
-  }); */
-});  
+  var pool = mysql.createPool({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    connectionLimit: 10,
+    multipleStatements: true
+  });
+
+  pool.getConnection((err: any, conn: any) => {
+    if(err){
+      console.log('Entered an error: ', err);
+      res.send({
+        success: false,
+        statusCode: 500,
+        message: 'Error during connection'
+      }) 
+      
+      return;
+    }
+
+    console.log('req.body: ' + req.body);
+    let sqlQuery = `call registeruser(?,?,?)`;
+    
+
+    conn.query(sqlQuery, [req.body.email, req.body.phone, req.body.password], (err: any, rows: any) => {
+      if(err){
+        console.log('Encountered an error: ', err);
+        conn.release();
+
+        return res.send({
+          success: false,
+          statusCode: 400
+        });      
+      }
+
+      res.send({
+        message: 'Successul',
+        statusCode: 200,
+        // data: rows
+      });
+
+      conn.release();   // close connection
+    });
+    
+  });
+});
 
 app.post('/id/:id/name/:name', (req: Request, res: Response) => {
   res.send({ 
